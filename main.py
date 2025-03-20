1097abea05eb635495e89c30229bf0f856262a58
Hayden Cole <haydencole@protonmail.com>
2025-03-20 14:11:19 -0700

    Add /tanks/{id} endpoint

--- main.py
+++ main.py
@@ -12,13 +12,14 @@ def get_db_connection():
 def list_endpoints():
     routes = []
     for rule in app.url_map.iter_rules():
-        if "GET" in rule.methods and rule.endpoint != "static":
+        if "GET" in rule.methods and rule.endpoint != "static" and "<int:id>" not in rule.rule:
             routes.append({
                 'url': rule.rule
             })
 
     html_content = "<h1>Tank.ly API Endpoints</h1>"
-    html_content += "<p>Click to view data.</p><ul>"
+    html_content += "<p>Click to view data.</p>"
+    html_content += "<p>See individual tanks by making a curl request or manually navigating to /tanks/{id}.</p><ul>"
     for route in routes:
         html_content += f'<li><a href="{route["url"]}">{route["url"]}</a></li>'
     html_content += "</ul>"
@@ -31,6 +32,20 @@ def get_tanks():
     tanks = conn.execute('SELECT tank.id, tank.name, country.name AS country, type.name AS type, tank.year_mfg FROM tank LEFT JOIN country ON country.id = tank.country_id LEFT JOIN type on type.id = tank.type_id').fetchall()
     conn.close()
     return jsonify([dict(row) for row in tanks])
+
+@app.route('/api/tanks/<int:id>', methods=['GET'])
+def get_tank(id):
+    conn = get_db_connection()
+    query = '''
+    SELECT tank.id, tank.name, country.name AS country, type.name AS type, tank.year_mfg
+    FROM tank
+    LEFT JOIN country ON country.id = tank.country_id
+    LEFT JOIN type ON type.id = tank.type_id
+    WHERE tank.id = ?
+    '''
+    tank = conn.execute(query, (id,)).fetchone()
+    conn.close()
+    return jsonify([dict(tank)])
 
 @app.route('/api/countries', methods=['GET'])
 def get_countries():
